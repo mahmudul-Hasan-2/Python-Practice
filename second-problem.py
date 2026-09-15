6b7c89b0b7f68fd345f238ddae06c658e0537675
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-09-15 21:09:49 +0600

    Solved second problems

---
+++ second-problem.py
@@ -0,0 +1,12 @@
+num1 = 3
+num2 = 7
+num3 = 1
+
+if num1 > num2 and num1 > num3:
+    print(f"The Max number is num1: {num1}")
+
+elif num2 > num1 and num2 > num3:
+    print(f"The Max number is num2: {num2}")
+
+elif num3 > num1 and num3 > num2:
+    print(f"The Max number is num3: {num3}")
